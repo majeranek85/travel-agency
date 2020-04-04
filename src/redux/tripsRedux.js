9ddc292531bd968443373d281e74f7e96c648285
Marta Majerowska <majeranek85@gmail.com>
2020-04-04 13:42:07 +0200

    Add duration trip filtration

--- src/redux/tripsRedux.js
+++ src/redux/tripsRedux.js
@@ -12,6 +12,9 @@ export const getFilteredTrips = ({trips, filters}) => {
   }
 
   // TODO - filter by duration
+  else if(filters.duration) {
+    output = output.filter(trip => (trip.days >= filters.duration.from && trip.days <= filters.duration.to));
+  }
 
   // TODO - filter by tags
 
