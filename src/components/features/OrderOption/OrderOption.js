83689a3b0be8801f0c59a35871723e674d0cca07
Marta Majerowska <majeranek85@gmail.com>
2020-04-09 22:33:15 +0200

    Update OrderOptionDropdown component

--- src/components/features/OrderOption/OrderOption.js
+++ src/components/features/OrderOption/OrderOption.js
@@ -16,18 +16,16 @@ const optionTypes = {
   number: OrderOptionNumber,
 };
 
-const OrderOption = ({name, type, ...otherProps}) => {
+const OrderOption = ({name, type, setOrderOption, id, ...otherProps}) => {
+
   const OptionComponent = optionTypes[type];
-  if(!OptionComponent){
+  if(!OptionComponent) {
     return null;
   } else {
-    console.log('OptionComponent: ', OptionComponent);
     return (
       <div className={styles.component}>
         <h3 className={styles.title}>{name}</h3>
-        <OptionComponent
-          {...otherProps}
-        />
+        <OptionComponent {...otherProps} setOrderValue={value => setOrderOption({[id]: value})} />
       </div>
     );
   }
@@ -36,6 +34,8 @@ const OrderOption = ({name, type, ...otherProps}) => {
 OrderOption.propTypes = {
   name: PropTypes.string,
   type: PropTypes.string,
+  setOrderOption: PropTypes.func,
+  id: PropTypes.string,
 };
 
 export default OrderOption;
