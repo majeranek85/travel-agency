8732cf843b20f0273f90c6afc7e49532a640f429
Marta Majerowska <majeranek85@gmail.com>
2020-04-14 14:56:13 +0200

    Update onChange attribute

--- src/components/features/OrderOption/OrderOptionText.js
+++ src/components/features/OrderOption/OrderOptionText.js
@@ -1,13 +1,18 @@
 import React from 'react';
-//import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
 
 import styles from './OrderOption.scss';
 
-const OrderOptionText = () => (
+const OrderOptionText = ({setOptionValue}) => (
   <input
     className={styles.input}
     placeholder='Please enter here'
+    onChange={event => setOptionValue(event.currentTarget.value)}
   />
 );
+
+OrderOptionText.propTypes = {
+  setOptionValue: PropTypes.func,
+};
 
 export default OrderOptionText;
