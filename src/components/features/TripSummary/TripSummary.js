fcac8dfa941a63c938327b516a0c642c415b79f0
Marta Majerowska <majeranek85@gmail.com>
2020-05-03 22:16:45 +0200

    Fix TripSummary test err

--- src/components/features/TripSummary/TripSummary.js
+++ src/components/features/TripSummary/TripSummary.js
@@ -7,28 +7,40 @@ import {formatPrice} from '../../../utils/formatPrice';
 import {promoPrice} from '../../../utils/promoPrice';
 import { parseOptionPrice } from '../../../utils/parseOptionPrice';
 
-const TripSummary = ({id, image, name, cost, days, tags, discount=20}) => (
-  <Col xs={12} sm={6} lg={4} className={styles.column}>
-    <Link to={`/trip/${id}`} className={styles.link}>
-      <article className={styles.component}>
-        <img src={image} alt={name} />
-        <h3 className={styles.title}>{name}</h3>
-        <div className={styles.details}>
-          <span>{days} days</span>
-          <span className={styles.promo}>Happy Hour price from: {promoPrice(formatPrice(cost), discount)}</span>
-          <span className={styles.standard}>Standard price: {formatPrice(parseOptionPrice(cost).value)}</span>
-        </div>
-        {tags == false ? null :(
-          <div className={styles.tags}>
-            {tags.map(tag => (
-              <span className={styles.tag} key={tag.toString()}>{tag}</span>
-            ))}
+const TripSummary = ({id, image, name, cost, days, tags, discount=20}) => {
+
+  const currentTime = new Date().getUTCHours();
+  console.log('currentTime', currentTime);
+
+
+  return(
+    <Col xs={12} sm={6} lg={4} className={styles.column}>
+      <Link to={`/trip/${id}`} className={styles.link}>
+        <article className={styles.component}>
+          <img src={image} alt={name} />
+          <h3 className={styles.title}>{name}</h3>
+          <div className={styles.details}>
+            <span>{days} days</span>
+            <span className={styles.promo}>
+              {currentTime == 20
+                ?(cost && discount) && (`Happy Hour price from: ${promoPrice(formatPrice(cost), discount)}`)
+                : null
+              }
+            </span>
+            <span className={styles.standard}>{(cost) && (`Standard price: ${formatPrice(parseOptionPrice(cost).value)}`)}</span>
           </div>
-        )}
-      </article>
-    </Link>
-  </Col>
-);
+          {tags == false ? null :(
+            <div className={styles.tags}>
+              {tags.map(tag => (
+                <span className={styles.tag} key={tag.toString()}>{tag}</span>
+              ))}
+            </div>
+          )}
+        </article>
+      </Link>
+    </Col>
+  );
+};
 
 TripSummary.propTypes = {
   id: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
